9ec918bcaa6eb71e187bfaa88e1619e60c77f63a
GregorySwart <gregory.swart97@gmail.com>
2026-06-09 20:31:16 +0100

    Add moveset for remaining pieces

--- black_bishop.gd
+++ black_bishop.gd
@@ -13,8 +13,15 @@ func _process(delta: float) -> void:
 	super(delta)
 
 func get_moves(_board_dict: Dictionary[String, Piece]) -> Array[String]:
-	# TODO Implement move logic
-	var _potential_moves: Array[String] = []
-	var _current_square: Vector2i = Utils.alg_to_board_coords(square_alg)
+	var potential_moves: Array[String] = []
+	var current_square: Vector2i = Utils.alg_to_board_coords(square_alg)
 	
-	return []
+	for displacement_factor: int in range(1, 7):
+		for direction_unit_vector: Vector2i in [Vector2i(1, 1), Vector2i(1, -1), Vector2i(-1, 1), Vector2i(-1, -1)]:
+			var displacement_vector: Vector2i = displacement_factor * direction_unit_vector
+			var target_square: Vector2i = current_square + displacement_vector
+			if 1 <= target_square.x and target_square.x <= 8 and 1 <= target_square.y and target_square.y <= 8:
+				var target_square_alg: String = Utils.board_coords_to_alg(target_square)
+				potential_moves.append(target_square_alg)
+		
+	return potential_moves
